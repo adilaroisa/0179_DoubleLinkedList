1a87ca15f1dfb64bcf76a81e94d48cd833bc19d3
adilaroisa <adilaroisa688@gmail.com>
2024-05-02 22:45:56 +0700

    melanjutkan membuat fungsi deleteNode

--- DoubleLinkedList2Mei2024/DoubleLinkedList2Mei2024.cpp
+++ DoubleLinkedList2Mei2024/DoubleLinkedList2Mei2024.cpp
@@ -95,6 +95,19 @@ void deleteNode()
         return;
     }
 
+    current = START; // step 1: start from the first node
+    previous = NULL;
+
+    // locate the node to be deleted
+    while (current != NULL && current->noMhs != rollNo)
+    {
+        previous = current;
+        current = current->next;
+    }
+
+    if (current == NULL)
+    {
+
 }
 
 
