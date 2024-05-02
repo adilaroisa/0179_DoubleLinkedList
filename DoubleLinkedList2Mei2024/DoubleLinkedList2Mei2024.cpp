b24c3d5f76a85016059ecd372b6ff9b1092938d2
adilaroisa <adilaroisa688@gmail.com>
2024-05-02 22:40:23 +0700

    melanjutkan membuat bool search

--- DoubleLinkedList2Mei2024/DoubleLinkedList2Mei2024.cpp
+++ DoubleLinkedList2Mei2024/DoubleLinkedList2Mei2024.cpp
@@ -73,6 +73,12 @@ bool seacrh(int rollNo, Node** previous, Node** current)
 {
     *previous = NULL;
     *current = START;
+    while (*current != NULL && (*current)->noMhs != rollNo)
+    {
+        *previous = *current;
+        *current = (*current)->next;
+    }
+    return (*current != NULL);
 }
 
 
