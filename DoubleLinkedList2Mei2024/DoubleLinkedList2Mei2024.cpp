0da1b356babfa6d41933aefaa20a7b1a9ddd2d3e
adilaroisa <adilaroisa688@gmail.com>
2024-05-02 22:44:12 +0700

    melanjutkan membuat fungsi deleteNode

--- DoubleLinkedList2Mei2024/DoubleLinkedList2Mei2024.cpp
+++ DoubleLinkedList2Mei2024/DoubleLinkedList2Mei2024.cpp
@@ -85,6 +85,16 @@ void deleteNode()
 {
     Node* previous, * current;
     int rollNo;
+
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    cin >> rollNo; // step 3: get the roll number number to be deleted
+
+    if (START == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
 }
 
 
