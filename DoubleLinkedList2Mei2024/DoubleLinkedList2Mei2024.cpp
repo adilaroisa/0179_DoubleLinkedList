77550f04a180236409a53188c93bf0317580cc55
adilaroisa <adilaroisa688@gmail.com>
2024-05-02 22:31:00 +0700

    melanjutkan membuat addNode

--- DoubleLinkedList2Mei2024/DoubleLinkedList2Mei2024.cpp
+++ DoubleLinkedList2Mei2024/DoubleLinkedList2Mei2024.cpp
@@ -24,6 +24,11 @@ void addNode() {
     if (START == NULL || newNode->noMhs <= START->noMhs)
     { // step 2: insert the new node at the beginning
 
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+            return;
+        }
 
 
 
