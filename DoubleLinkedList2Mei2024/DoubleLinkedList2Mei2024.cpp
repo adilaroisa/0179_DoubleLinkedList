7a56bd26ee98d62281fff32cd60148385af91043
adilaroisa <adilaroisa688@gmail.com>
2024-05-02 22:36:08 +0700

    melanjutkan membuat newNode

--- DoubleLinkedList2Mei2024/DoubleLinkedList2Mei2024.cpp
+++ DoubleLinkedList2Mei2024/DoubleLinkedList2Mei2024.cpp
@@ -37,8 +37,37 @@ void addNode() {
         newNode->prev = NULL; // step 5: make the new node point to NULL 
         START = newNode; // step 6: make the new node the first node
     }
+    else
+    {
+        // insert the new node in the middle or at the end
+        Node* current = START; // step 1.a: start from the first node
+        Node* previous = NULL; // step 1.b: previous node is Null initially
 
 
+        while (current != NULL && current->noMhs)
+        {                       // step 1.c: traverse the list to find the correct positionn
+            previous = current; // step 1.d: move the previous to the current node
+            current = current->next; // step 1.e: move the current to the next node
+        }
+
+        newNode->next = current;  // step 4: Make the next field of the new node point to current
+        newNode->prev = previous; // step 5: Make the previous field of the new node point to previous
+
+        if (current != NULL)
+        {
+            current->prev = newNode;  // step 6: Make the previous field of the current node point to the new node
+        }
+
+        if (previous != NULL)
+        {
+            previous->next = newNode; // step 7: Mak the next field of the previous node point to the new node
+        }
+        else {
+            // if previous is still NULL, it means newNode is now the first node
+            START = newNode;
+        }
+    }
+}
 
 
 
